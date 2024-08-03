f81005c905dca2692784a01bc5a96694413ea731
Ahmed Esmail <97197954+AhmedEsmail8@users.noreply.github.com>
2024-08-03 20:09:12 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/start-framework-website/',
-  server: {
-    port: process.env.PORT as unknown as number,
-  },
   plugins: [react()],
 });
